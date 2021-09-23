ed30fb29465531b431310fbb08394d67e97e805e
Tan Jiann Meng <jiannmeng@gmail.com>
2021-09-23 16:08:51 +0800

    Refactor with nicer names

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import asyncio
 import logging
+import re
 from pathlib import Path
 from urllib.parse import quote
+from bs4.element import NavigableString
 
 import httpx
 from bs4 import BeautifulSoup
-from rich import print
 from rich.logging import RichHandler
 
 logging.basicConfig(
@@ -20,19 +21,26 @@ XML_FOLDER = Path(__file__).resolve().parent.parent / "xml"
 BROWSE_URL = "https://fast.bnm.gov.my/fastweb/public/FastPublicBrowseServlet.do"
 INFO_URL = "https://fast.bnm.gov.my/fastweb/public/PublicInfoServlet.do"
 
+MAX_CONCURRENT_TASKS = 8  # Any higher is too much for BNM's servers...
+HTTP_TIMEOUT_SECS = 30
+
+
+class WebsiteError(Exception):
+    pass
+
 
 # 200500000021æ07052021æ00000088èCorporate Bond
-def to_xml_name(id: str) -> str:
+def to_xml_name(fast_id: str) -> str:
     """Convert an encoded id from FAST website into filesystem-safe name with .xml
     extension.
 
     e.g. 200500000021æ14/09/2021æ00000088èCorporate Bond Ratingsè14/09/2021
     becomes 200500000021-14092021-00000088-Corporate Bond Ratings-14092021.xml
     """
-    return id.replace("/", "").replace("æ", "-").replace("è", "-") + ".xml"
+    return fast_id.replace("/", "").replace("æ", "-").replace("è", "-") + ".xml"
 
 
-def browse_formdata(page: int) -> dict[str, str]:
+def browse_form(page: int) -> dict[str, str]:
     return dict(
         configButton="false",
         drawCheckBox="Y",
@@ -51,7 +59,7 @@ def browse_formdata(page: int) -> dict[str, str]:
     )
 
 
-def info_formdata(data_id: str) -> str:
+def info_form_str(data_id: str) -> str:
     data = dict(
         chkBox=data_id,
         taskId="PB030100",
@@ -72,60 +80,91 @@ def info_formdata(data_id: str) -> str:
         txtDefaultCriteria="",
         txtSearchCriteria="",
     )
-
     data = {k: quote(v, encoding="windows-1252") for k, v in data.items()}
-
     x = "&".join("=".join(i) for i in data.items())
     return x
 
 
-async def main():
-    first_page = httpx.post(BROWSE_URL, data=browse_formdata(1), verify=False)
-    soup = BeautifulSoup(first_page.text)
-    last = soup.find("a", string="[Last]")
-    last_page_num = 3
-    # last_page_num = int(re.search(r"\d+", last["href"]).group(0))
+async def gather_with_concurrency(n: int, *tasks):
+    """Asyncio gather, but with a limit of how many tasks can run concurrently.
 
-    retreived_ids = set()
+    Ref: https://stackoverflow.com/a/61478547
+    """
+    semaphore = asyncio.Semaphore(n)
+
+    async def sem_task(task):
+        async with semaphore:
+            return await task
+
+    return await asyncio.gather(*(sem_task(task) for task in tasks))
+
+
+async def main():
+    # Load the Consolidated IYTM main page, and find the page number of the last page
+    page = httpx.post(BROWSE_URL, data=browse_form(1), verify=False)
+    soup = BeautifulSoup(page.text, features="html.parser")
+    last_elem = soup.find("a", string="[Last]")
+    if isinstance(last_elem, NavigableString) or last_elem is None:
+        raise WebsiteError("Could not find a DOM element containing string [Last]")
+    last_js = last_elem.attrs["href"]
+    last_match = re.search(r"\d+", last_js)
+    if last_match is None:
+        raise WebsiteError("DOM element containing [Last] did not have a page number")
+    last_page = int(last_match.group(0))
+    logger.info(f"Consolidated IYTM has {last_page} pages")
+
+    # Paginate through all the pages and determine the set of all valid FAST IDs
+    fast_ids = set()
 
     async def get_page(page_num: int):
-        async with httpx.AsyncClient(verify=False) as client:
-            print(f"page {page_num}")
-            response = await client.post(BROWSE_URL, data=browse_formdata(page_num))
+        async with httpx.AsyncClient(verify=False, timeout=HTTP_TIMEOUT_SECS) as client:
+            logger.info(f"Accessing page {page_num} of Consolidated IYTM listing")
+            response = await client.post(BROWSE_URL, data=browse_form(page_num))
             page = BeautifulSoup(response.text, features="html.parser")
             checkboxes = page.find_all("input", id="chkBox")
             # return [cb["values"] for cv in checkboxes]
-            retreived_ids.update(cb["value"] for cb in checkboxes)
+            fast_ids.update(cb["value"] for cb in checkboxes)  # type:ignore
 
-    tasks = [get_page(num) for num in range(1, last_page_num + 1)]
-    await asyncio.gather(*tasks)
+    tasks = [get_page(num) for num in range(1, last_page + 1)]
+    await gather_with_concurrency(MAX_CONCURRENT_TASKS, *tasks)
+    # At this point, fast_ids will have all of the ids available on FAST website
 
-    # deduplicate
-    downloaded_ids = set()
+    # Now, we check which ids we have already downloaded
+    downloaded_xmls = set()
     new_ids = set()
     for xml in XML_FOLDER.iterdir():
-        downloaded_ids.add(xml.name)
-    for data_id in retreived_ids:
-        if to_xml_name(data_id) not in downloaded_ids:
+        downloaded_xmls.add(xml.name)
+    for data_id in fast_ids:
+        if to_xml_name(data_id) not in downloaded_xmls:
             new_ids.add(data_id)
 
-    async def get_xml(data_id: str):
-        async with httpx.AsyncClient(verify=False) as client:
-            print(f"downloading {data_id}")
+    # Download all of the remaining xml files, and save to disk
+    async def get_xml(fast_id: str):
+        async with httpx.AsyncClient(verify=False, timeout=HTTP_TIMEOUT_SECS) as client:
+            logger.info(f"Downloading new id: {fast_id}")
 
+            # httpx requests using the data parameter sends a url-encoded form with
+            # utf-8 encoding. Unfortunately, the FAST id uses Windows-1252 encoding,
+            # so we need to send a properly encoded string and the appropriate headers
+            # instead.
             response = await client.post(
                 INFO_URL,
-                content=info_formdata(data_id),
+                content=info_form_str(fast_id),
                 headers={
                     "Content-Type": "application/x-www-form-urlencoded",
                     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
                 },
             )
-            with open(XML_FOLDER / to_xml_name(data_id), "wb") as f:
+
+            # Save the file to the xml folder
+            xml_path = XML_FOLDER / to_xml_name(fast_id)
+            with open(xml_path, "wb") as f:
                 f.write(response.content)
+            logger.info(f"XML saved to {xml_path}")
 
     tasks = [get_xml(data_id) for data_id in new_ids]
-    await asyncio.gather(*tasks)
+    await gather_with_concurrency(MAX_CONCURRENT_TASKS, *tasks)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,3 @@
-import pytest
 from src.main import to_xml_name
 
 
