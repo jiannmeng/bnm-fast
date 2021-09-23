aed663e75b52999aab9732188f9ff33894fe3dff
Tan Jiann Meng <jiannmeng@gmail.com>
2021-09-23 12:18:29 +0800

    Async httpx requests and saving of xml files

--- src/main.py
+++ src/main.py
@@ -1,27 +1,32 @@
-import requests
+import httpx
 from bs4 import BeautifulSoup
 from pathlib import Path
 import logging
+import re
 from rich import print
+import asyncio
 
 from rich.logging import RichHandler
+from urllib.parse import quote
 
 logging.basicConfig(
-    level="NOTSET",
+    level="DEBUG",
     format="%(message)s",
     datefmt="[%X]",
-    handlers=[RichHandler(rich_tracebacks=True)]
+    handlers=[RichHandler(rich_tracebacks=True)],
 )
 logger = logging.getLogger("rich")
 
-XML_FOLDER = Path("../xml")
+XML_FOLDER = Path(__file__).resolve().parent.parent / "xml"
 CONSOLIDATED_IYTM_URL = "https://fast.bnm.gov.my/fastweb/public/FastPublicBrowseServlet.do?mode=MAIN&taskId=PB030100"
 BROWSE_URL = "https://fast.bnm.gov.my/fastweb/public/FastPublicBrowseServlet.do"
 INFO_URL = "https://fast.bnm.gov.my/fastweb/public/PublicInfoServlet.do"
 
-# disable ssl warnings
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# 200500000021æ07052021æ00000088èCorporate Bond
+def format_data_id(data_id: str) -> str:
+    return data_id.replace("/", "").replace("æ", "-").replace("è", "-") + ".xml"
+
 
 def browse_formdata(page: int) -> dict[str, str]:
     return dict(
@@ -41,8 +46,9 @@ def browse_formdata(page: int) -> dict[str, str]:
         txtStartPageIndex="1",
     )
 
-def info_formdata(data_id: str) -> dict[str, str]:
-    return dict(
+
+def info_formdata(data_id: str) -> str:
+    data = dict(
         chkBox=data_id,
         taskId="PB030100",
         mode="FIND",
@@ -63,23 +69,59 @@ def info_formdata(data_id: str) -> dict[str, str]:
         txtSearchCriteria="",
     )
 
-def main():
-    page_num = 1
-    data_ids = set()
-    while True:
-        resp = requests.post(BROWSE_URL, data=browse_formdata(page_num), verify=False)
-        page = BeautifulSoup(resp.text)
-        checkboxes = page.find_all("input", id="chkBox")
-        data_ids.update(cb["value"] for cb in checkboxes)
-        next_page = page.find("a", string="[Next]")
-        if next_page is None:
-            break
-        else:
-            page_num += 1
-        print(page_num)
-    print(data_ids)
-    pass
+    data = {k: quote(v, encoding="windows-1252") for k, v in data.items()}
 
+    x = "&".join("=".join(i) for i in data.items())
+    return x
+
+
+async def main():
+    first_page = httpx.post(BROWSE_URL, data=browse_formdata(1), verify=False)
+    soup = BeautifulSoup(first_page.text)
+    last = soup.find("a", string="[Last]")
+    last_page_num = 3
+    # last_page_num = int(re.search(r"\d+", last["href"]).group(0))
+
+    retreived_ids = set()
+
+    async def get_page(page_num: int):
+        async with httpx.AsyncClient(verify=False) as client:
+            print(f"page {page_num}")
+            response = await client.post(BROWSE_URL, data=browse_formdata(page_num))
+            page = BeautifulSoup(response.text, features="html.parser")
+            checkboxes = page.find_all("input", id="chkBox")
+            # return [cb["values"] for cv in checkboxes]
+            retreived_ids.update(cb["value"] for cb in checkboxes)
+
+    tasks = [get_page(num) for num in range(1, last_page_num + 1)]
+    await asyncio.gather(*tasks)
+
+    # deduplicate
+    downloaded_ids = set()
+    new_ids = set()
+    for xml in XML_FOLDER.iterdir():
+        downloaded_ids.add(xml.name)
+    for data_id in retreived_ids:
+        if format_data_id(data_id) not in downloaded_ids:
+            new_ids.add(data_id)
+
+    async def get_xml(data_id: str):
+        async with httpx.AsyncClient(verify=False) as client:
+            print(f"downloading {data_id}")
+
+            response = await client.post(
+                INFO_URL,
+                content=info_formdata(data_id),
+                headers={
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                },
+            )
+            with open(XML_FOLDER / format_data_id(data_id), "wb") as f:
+                f.write(response.content)
+
+    tasks = [get_xml(data_id) for data_id in new_ids]
+    await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
